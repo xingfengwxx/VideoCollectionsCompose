3ca7af5d97f4f1a20607418341477716a9c93795
WangXingxing <1099420259@qq.com>
2025-04-30 16:56:10 +0800

    feat(splash): 重构启动屏页面并添加新动画效果
    - 移除 Lottie 动画，改用 Compose 动画
    - 添加淡入和缩放动画效果
    - 更新启动屏背景图和 logo
    - 新增启动屏标语文本
    - 优化启动屏样式和布局

--- app/src/main/java/com/wangxingxing/videocollectionscompose/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/wangxingxing/videocollectionscompose/ui/splash/SplashScreen.kt
@@ -1,18 +1,32 @@
 package com.wangxingxing.videocollectionscompose.ui.splash
 
+import android.view.animation.AlphaAnimation
+import android.view.animation.ScaleAnimation
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Animation
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.animateLottieCompositionAsState
-import com.airbnb.lottie.compose.rememberLottieComposition
 import com.wangxingxing.videocollectionscompose.R
 
 /**
@@ -26,37 +40,70 @@ fun SplashScreen(
     modifier: Modifier = Modifier,
     onFinish: () -> Unit
 ) {
+    val alpha = remember { Animatable(0.5f) }
+    val scale = remember { Animatable(1f) }
 
-    // 加载 Lottie 动画
-    val composition by rememberLottieComposition(
-        spec = LottieCompositionSpec.RawRes(R.raw.anim_splash)
-    )
+    val animDuration = 3000
 
-    // 监听动画进度
-    val progress by animateLottieCompositionAsState(
-        composition = composition,
-        iterations = 1, // 只播放一次
-        isPlaying = true,
-        speed = 2f
-    )
-
-    // 监听动画是否结束
-    LaunchedEffect(progress) {
-        if (progress >= 1f) { // 动画播放结束
-            // 跳转到 MainScreen
-            onFinish()
-        }
+    // 启动淡入和缩放动画
+    LaunchedEffect(Unit) {
+        // 同时启动两个动画
+        alpha.animateTo(
+            targetValue = 1.0f, // 目标透明度
+            animationSpec = tween(
+                durationMillis = animDuration,
+                easing = FastOutSlowInEasing // 先快后慢的插值器
+            )
+        )
+        scale.animateTo(
+            targetValue = 1.2f, // 目标缩放比例
+            animationSpec = tween(
+                durationMillis = animDuration,
+                easing = FastOutSlowInEasing // 先快后慢的插值器
+            )
+        )
+        onFinish() // 动画结束后调用onFinish
     }
 
     Box(
         modifier = modifier.fillMaxSize()
     ) {
-        LottieAnimation(
-            composition = composition,
-            iterations = Int.MAX_VALUE, // 无限循环播放
+        Image(
+            painter = painterResource(R.drawable.bg_splash),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
             modifier = Modifier
-                .size(200.dp) // 设置动画大小
-                .align(Alignment.Center) // 居中显示
+                .fillMaxSize()
+                .graphicsLayer {
+                    scaleX = scale.value
+                    scaleY = scale.value
+                    this.alpha = alpha.value
+                }
+        )
+
+        Image(
+            painter = painterResource(R.drawable.ic_logo_slogan),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 236.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.txt_splash_slogan),
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 36.dp)
         )
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen(
+        onFinish = {}
+    )
 }
